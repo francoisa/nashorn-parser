d4f29363a570799a78c77d9ba7d9587d11f564c1
francoisa <andre.francois@gmail.com>
2016-08-10 18:27:38 -0400

    Add fs.js and move code generation script.

--- generate-code.cmd
+++ generate-code.cmd
@@ -1,5 +1,7 @@
 @echo off
 setlocal
 set CLASSPATH=D:\java\antlr4-4.5.3\tool\target\antlr4-4.5.4-SNAPSHOT.jar
+cd src
+copy ..\contrib\fs.js node_modules\.
 java org.antlr.v4.Tool -Dlanguage=JavaScript arithmetic.g4
 endlocal
